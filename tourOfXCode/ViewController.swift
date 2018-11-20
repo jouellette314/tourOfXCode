f08c2e79eadea185cc1460e15ef2ac42cae7555f
Justin Ouellette <jouellette314@justins-macbook-pro.local>
2018-11-20 10:13:41 -0800

    Initial Commit

---
+++ tourOfXCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tourOfXCode
+//
+//  Created by Justin Ouellette on 11/20/18.
+//  Copyright © 2018 Justin Ouellette. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
